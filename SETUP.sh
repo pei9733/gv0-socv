bc40596e66bd807c210654d4188d2aead803be9d
pei9733 <84021157+pei9733@users.noreply.github.com>
2023-05-11 20:02:58 +0800

    Update SETUP.sh -- add '-y'

--- SETUP.sh
+++ SETUP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install prerequisites for building yosys
-sudo apt-get install gperf build-essential bison flex libreadline-dev gawk tcl-dev libffi-dev graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev
+sudo apt-get install -y gperf build-essential bison flex libreadline-dev gawk tcl-dev libffi-dev graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev
 
 # Make install yosys
 if [ -f "/usr/local/lib/yosys/libyosys.so" ]; then
@@ -13,4 +13,4 @@ else
 fi
 
 # Install GTKWave package
-sudo apt-get install gtkwave;
+sudo apt-get install -y gtkwave;
